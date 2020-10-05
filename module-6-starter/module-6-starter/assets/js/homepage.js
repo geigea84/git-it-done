9207a9d94c18d867301d06e5359790fa7ba1de99
geigea84 <geigea84@gmail.com>
2020-10-04 22:19:09 -0500

    finished 6.1

--- module-6-starter/module-6-starter/assets/js/homepage.js
+++ module-6-starter/module-6-starter/assets/js/homepage.js
@@ -1,5 +1,19 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
-};
+/*6.1.5 The then() method returns a Promise. It takes up to two arguments: 
+callback functions for the success and failure cases of the Promise.*/
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/then
+/*Notice how the response object in your fetch() logic has a method called json(). 
+This method formats the response as JSON. But sometimes a resource may return non-
+JSON data, and in those cases, a different method, like text(), would be used.*/
+var getUserRepos = function (user) {
+    //format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
+    //make a request to the url
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
+}
+//enter usernames with quotes inside parenthesis
 getUserRepos();
